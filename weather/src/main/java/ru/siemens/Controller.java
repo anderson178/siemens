595e05a6ec43c541110d49d16c9e70936f8e27b6
anderson178 <denismironenko@yandex.ru>
2019-04-21 19:40:27 +0300

    spting

--- weather/src/main/java/ru/siemens/Controller.java
+++ weather/src/main/java/ru/siemens/Controller.java
@@ -1,26 +1,34 @@
 package ru.siemens;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/map")
+
+
 public class Controller {
+
+
+    @Qualifier("jdbcTemplate")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
 
     @GetMapping
     public List<String> getStr() {
-       //JdbcTemplate jdbc = new JdbcTemplate();
-       // List<String> list = jdbc.queryForList("SELECT WIDTH FROM TEMPERATURE_INDICATORS", String.class);
-        return new JdbcTemplate().queryForList("SELECT WIDTH FROM TEMPERATURE_INDICATORS", String.class);
-       //return "wwwwwwwww";
+        return jdbcTemplate.queryForList("SELECT WIDTH FROM TEMPERATURE_INDICATORS", String.class);
     }
+
 
     /*@PostMapping
     public void postStr(String str) {
