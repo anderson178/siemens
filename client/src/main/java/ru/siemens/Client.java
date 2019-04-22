78732905ffad507987293c6fd43f6ff03e33ce7b
anderson178 <denismironenko@yandex.ru>
2019-04-22 13:35:38 +0300

    get client and getMapping controller

---
+++ client/src/main/java/ru/siemens/Client.java
@@ -0,0 +1,19 @@
+package ru.siemens;
+
+import org.springframework.web.client.RestTemplate;
+
+public class Client {
+    private static final String URL_EMPLOYES = "http://127.0.0.1:8080/map";
+
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String[] rst = restTemplate.getForObject(URL_EMPLOYES, String[].class);
+        if (rst != null) {
+            for (String data : rst) {
+                System.out.println(data);
+            }
+        }
+
+    }
+}
--- weather/src/main/java/ru/siemens/Controller.java
+++ weather/src/main/java/ru/siemens/Controller.java
@@ -4,11 +4,13 @@ package ru.siemens;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,17 +18,22 @@ import java.util.List;
 
 
 public class Controller {
-
+    private static final String QUERYSQL = "SELECT WIDTH, LONGITUDE, TEMPERATURE,DATATIME FROM TEMPERATURE_INDICATORS order by DATATIME desc LIMIT 2";
+    private SqlRowSet srs;
 
     @Qualifier("jdbcTemplate")
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
 
-
     @GetMapping
     public List<String> getStr() {
-        return jdbcTemplate.queryForList("SELECT WIDTH FROM TEMPERATURE_INDICATORS", String.class);
+        srs = jdbcTemplate.queryForRowSet(QUERYSQL);
+        List<String> rst = new ArrayList<>();
+        while (srs.next()) {
+            rst.add(srs.getString("WIDTH") + " " + srs.getString("LONGITUDE") + " " + srs.getInt("TEMPERATURE") + " " + srs.getDate("DATATIME"));
+        }
+        return rst;
     }
 
 
