be41ef4281ded4cf8a7ed0599fa89452f7678a1f
anderson178 <denismironenko@yandex.ru>
2019-04-24 22:59:03 +0300

    test controller  - get refactoring

--- weather/src/test/java/ru/siemens/ControllerTest.java
+++ weather/src/test/java/ru/siemens/ControllerTest.java
@@ -1,5 +1,6 @@
 package ru.siemens;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -9,46 +10,44 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=Application.class)
 @TestPropertySource(locations="classpath:application.properties")
 public class ControllerTest {
     private static final String QUERYGET = "SELECT WIDTH, LONGITUDE, TEMPERATURE,DATATIME FROM TEMPERATURE_INDICATORS order by DATATIME desc LIMIT 5";
+    private ResultSet resultSet;
     @Autowired
     private Controller controller;
-
-    @Test
-    public void whenGetMapping() throws Exception {
+    @Before
+    public void aa() throws SQLException, ClassNotFoundException {
         Connection connection = null;
         Statement statement = null;
         Class.forName("org.postgresql.Driver");
         connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/weather", "postgres", "password123");
         statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(QUERYGET);
+        resultSet = statement.executeQuery(QUERYGET);
+    }
+
+
+    @Test
+    public void whenGetMapping() throws SQLException {
         List<String> list = new ArrayList<>();
         while (resultSet.next()) {
             list.add(resultSet.getDouble("width") + " " + resultSet.getDouble("longitude")
                     + " " + resultSet.getInt("temperature")
                     + " " + resultSet.getDate("datatime") + " " + resultSet.getTime("datatime"));
-
-
         }
         assertThat(controller.getStr(), is(list));
-
-
     }
+
 
 
 }
